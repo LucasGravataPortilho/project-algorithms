5f4c574cdfd260b10a73bc827a0801a99cf63a95
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-01 16:59:20 -0300

    requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums is None or len(nums) <= 1:
+        return False
+
+    numeros_iguais = []
+
+    for numero in nums:
+        if isinstance(numero, str) or numero < 0:
+            return False
+        if numero in numeros_iguais:
+            return numero
+        numeros_iguais.append(numero)
+
+    return False
