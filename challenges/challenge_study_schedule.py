e01889231e36ecdf5b809a16be900d9938c96198
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-01 15:18:58 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,22 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def verificarParam(param):
+    if (param is None):
+        return True
+    if isinstance(param, str):
+        return True
+    
+    return False
 
-#iniciando o projeto
+def study_schedule(permanence_period, target_time):
+    if target_time is None:
+        return None
+    
+    contador = 0
+
+    for period in permanence_period:
+        if verificarParam(period[0]) or verificarParam(period[1]):
+            return None
+        
+        if target_time >= period[0] and target_time <= period[1]:
+            contador += 1
+
+    return contador
