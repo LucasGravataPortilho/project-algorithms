8687ffe5f6cff88c567548cb24c0efdcbb7b1088
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-01 15:35:07 -0300

    fix do lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,14 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
-    
+
     if len(word) == 1:
         return True
-    
+
     if word[low_index] != word[high_index]:
         return False
-    
+
     if high_index - low_index <= 1:
         return True
-    
+
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,19 +3,20 @@ def verificarParam(param):
         return True
     if isinstance(param, str):
         return True
-    
+
     return False
+
 
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    
+
     contador = 0
 
     for period in permanence_period:
         if verificarParam(period[0]) or verificarParam(period[1]):
             return None
-        
+
         if target_time >= period[0] and target_time <= period[1]:
             contador += 1
 
