3d1581c0c08f923f8bda1e5e46cceca3b5107e98
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-01 17:12:39 -0300

    refactor requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,13 +2,13 @@ def find_duplicate(nums):
     if nums is None or len(nums) <= 1:
         return False
 
-    numeros_iguais = []
+    numeros_iguais = set()
 
     for numero in nums:
         if isinstance(numero, str) or numero < 0:
             return False
         if numero in numeros_iguais:
             return numero
-        numeros_iguais.append(numero)
+        numeros_iguais.add(numero)
 
     return False
